ebdbf7525ee9eac8c1f7175bcf5571d3eabc8884
Umut <57362066+umutakpinar@users.noreply.github.com>
2023-07-20 15:52:26 +0300

    Initial Commit

---
+++ VakifInternship_2/view/MainPageView.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace VakifInternship_2.view
+{
+    partial class MainPageView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainPageView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 617);
+            this.Name = "MainPageView";
+            this.Text = "MainPageView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ VakifInternship_2/view/MainPageView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VakifInternship_2.view
+{
+    public partial class MainPageView : Form
+    {
+        public MainPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
